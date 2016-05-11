076ef638ca984be07617ea998c33d63f15d9e6ac
Jacques Uys <jaxuys@gmail.com>
2016-05-11 15:50:46 +0200

    basecamp

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fundlistApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fundlistApp
+ */
+angular.module('fundlistApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
