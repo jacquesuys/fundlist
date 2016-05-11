dccc758f81816b43f14eb7a9f7951699b95a179a
Jacques Uys <jaxuys@gmail.com>
2016-05-11 16:52:50 +0200

    scope injection into controller

--- app/scripts/controllers/funds.js
+++ app/scripts/controllers/funds.js
@@ -8,8 +8,9 @@
  * Controller of the fundlistApp
  */
 angular.module('fundlistApp')
-  .controller('FundsCtrl', function (fundsService) {
+  .controller('FundsCtrl', function ($scope, fundsService) {
     fundsService.then(function(result){
       console.log(result);
+      $scope.funds = result;
     });
   });
